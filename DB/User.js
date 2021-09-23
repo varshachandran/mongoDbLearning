fde12b3ce38612636c148b8b891a8ab9795a310a
varshachandran <varshachandranck@gmail.com>
2021-09-23 19:36:12 +0530

    Create User.js

---
+++ DB/User.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+
+const user=new mongoose.Schema({
+    firstName:{
+        type:String
+    },
+    lastName:{
+        type:String
+    }
+})
+
+module.exports=User=mongoose.model('user',user);
+ 
